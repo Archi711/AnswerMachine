5e7e542fb5014b36cd9f98b371079462c7cf72e0
Archi711 <mlody711@o2.pl>
2020-03-22 09:38:43 +0100

    added styling to QF

--- src/components/QuestionForm/QuestionForm.tsx
+++ src/components/QuestionForm/QuestionForm.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
-
+import './QuestionForm.css';
 
 export const QuestionForm = () => {
   return (
-    <div>QuestionForm</div>
+    <form className="QFcontainer">
+      <label htmlFor="question" className="QFcontainer__label">Your question:</label>
+      <input
+        id="question"
+        className="QFcontainer__input"
+        type="text"
+      />
+      <button
+        className="QFcontainer__button QFcontainer__button--questionMark">
+        ?
+      </button>
+      <button
+        className="QFcontainer__button QFcontainer__button--submit">
+        ASK!
+      </button>
+    </form>
   )
 }
